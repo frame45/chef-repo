5a28a8f4fd84ccc19371cd0e2bac60c99aa4d2cf
Matt Yakel <matt.yakel@quibids.com>
2014-12-17 09:03:07 -0600

    Add all my files so far.

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+default["apache"]["sites"]["mattyakel2"] = { "site_title" => "Matt's Website Coming Soon", "port" => 80, "domain" => "mattyakel2.mylabserver.com" }
+default["apache"]["sites"]["mattyakel2b"] = { "site_title" => "Matt's Wesite B Coming Soon", "port" => 80, "domain" => "mattyakel2b.mylabserver.com" }
+
