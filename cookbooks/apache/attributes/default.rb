4306f06bf18cb99185e4841a7971110019ee9523
Matt Yakel <matt.yakel@quibids.com>
2014-12-31 17:07:43 -0600

    Update Apache Cookbook, Add environments.

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,3 +1,12 @@
-default["apache"]["sites"]["mattyakel2"] = { "site_title" => "Matt's Website Coming Soon", "port" => 80, "domain" => "mattyakel2.mylabserver.com" }
+default["apache"]["sites"]["mattyakel2"] = { "site_title" => "Centos Node Website", "port" => 80, "domain" => "mattyakel2.mylabserver.com" }
 default["apache"]["sites"]["mattyakel2b"] = { "site_title" => "Matt's Wesite B Coming Soon", "port" => 80, "domain" => "mattyakel2b.mylabserver.com" }
+default["apache"]["sites"]["mattyakel3"] = { "site_title" => "mattyakel3 website", "port" => 80, "domain" => "mattyakel3.mylabserver.com" }
 
+case node["platform"]
+  when "centos"
+    default["apache"]["package"] = "httpd"
+  when "ubuntu"
+    default["apache"]["package"] = "apache2"
+end
+
+default["author"]["name"] = "mattyakel"
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -7,43 +7,67 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package "httpd" do
-	action :install
+if node["platform"] == "ubuntu"
+  execute "apt-get update -y" do
+  end
+end
+
+package "apache2" do
+  package_name node["apache"]["package"] 
 end
 
 node["apache"]["sites"].each do |sitename, data|
   document_root = "/content/sites/#{sitename}"
-  
+
   directory document_root do 
-	mode "0755"
-	recursive true
+    mode "0755"
+    recursive true
   end
   
-template "/etc/httpd/conf.d/#{sitename}.conf" do
-	source "vhost.erb"
-	mode "0644"
-	variables(
-		:document_root => document_root,
-		:port	=> data["port"],
-		:domain => data["domain"]
-	)
-	notifies :restart, "service[httpd]"
+if node["platform"] == "ubuntu"
+  template_location = "/etc/apache2/sites-enabled/#{sitename}.conf"
+elsif node["platform"] == "centos"
+  template_location = "/etc/httpd/conf.d/#{sitename}.conf"
+end
+
+template template_location do
+  source "vhost.erb"
+  mode "0644"
+  variables(
+    :document_root => document_root,
+    :port	=> data["port"],
+    :domain => data["domain"]
+  )
+  notifies :restart, "service[httpd]"
 end
 
 template "/content/sites/#{sitename}/index.html" do
-	source "index.html.erb"
-	mode "0644"
-	variables(
-		:site_title => data["site_title"],
-		:comingsoon => "Coming Soon!"
-	)
+  source "index.html.erb"
+  mode "0644"
+  variables(
+    :site_title => data["site_title"],
+    :comingsoon => "Coming Soon!",
+    :author_name => node["author"]["name"]
+  )
 end
-
 end 
 
-service "httpd" do
-	action [:enable, :start]
+execute "rm /etc/httpd/conf.d/welcome.conf" do
+  only_if do
+    File.exist?("/etc/httpd/conf.d/welcome.conf")
+  end
+  notifies :restart, "service[httpd]"
 end
 
-include_recipe "php::default"
+execute "rm /etc/httpd/conf.d/README" do
+  only_if do
+    File.exist?("/etc/httpd/conf.d/README")
+  end
+end
 
+service "httpd" do
+  service_name node["apache"]["package"] 
+  action [:enable, :start]
+end
+
+#include_recipe "php::default"
